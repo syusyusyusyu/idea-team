5a3b9716015bd5c0bc9a81547d55dcea82a5b133
吉田柊 <C2375@oic.jp>
2024-04-21 05:55:42 +0900

    n

--- script.js
+++ script.js
@@ -7,31 +7,19 @@ const memberInput = document.getElementById("member");
 const mentorInput = document.getElementById("mentor");
 const resultDiv = document.getElementById('resultDiv');
 
-teamButton.addEventListener("click", function () {
+teamForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
     const leader = leaderInput.value;
     const members = memberInput.value.split('\n');
     const mentor = mentorInput.value;
 
-    if (allMembers.includes(leader)) {
-        alert("リーダーは既に他のチームのメンバーです");
+    if (allMembers.includes(leader) || members.some(member => allMembers.includes(member)) || allMembers.includes(mentor)) {
+        alert("一人以上のメンバーが既に他のチームのメンバーです");
         return;
     }
 
-    for (let i = 0; i < members.length; i++) {
-        if (allMembers.includes(members[i])) {
-            alert("一人以上のメンバーが既に他のチームのメンバーです");
-            return;
-        }
-    }
-
-    if (allMembers.includes(mentor)) {
-        alert("メンターは既に他のチームのメンバーです");
-        return;
-    }
-
-    allMembers.push(leader);
-    allMembers = allMembers.concat(members);
-    allMembers.push(mentor);
+    allMembers.push(leader, ...members, mentor);
 
     resultDiv.innerHTML = `
         <h2>チーム情報</h2>
